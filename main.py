f5dc2c53ae51158b7749ccf3a1608d1eb5c68f69
Jeyaprajan V <jeyaprajanv@gmail.com>
2026-08-14 04:24:43 +0530

    Allow deployed frontend CORS origin

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+    "http://localhost:5173",
+    "https://gogig-media-processing-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
